75abeb2df1462105e798583f5c053fd87487fcd7
dmngo <164151923+DMNgoboki@users.noreply.github.com>
2024-06-28 21:22:14 +0800

    w

--- me/src/components/AnimatedFish.tsx
+++ me/src/components/AnimatedFish.tsx
@@ -1,10 +1,10 @@
 "use client";
 
+import "../styles/AnimatedFish.css";
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
-import "../styles/AnimatedFish.css";
 
 gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
 
