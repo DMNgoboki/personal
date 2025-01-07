9f73267023b08a124785179ff2962861a0d7e1bc
dmngo <164151923+DMNgoboki@users.noreply.github.com>
2025-01-07 17:53:44 +0800

    up

--- me/src/app/contact/page.tsx
+++ me/src/app/contact/page.tsx
@@ -1,24 +1,61 @@
 "use client";
 
 import { Header } from "@/components/header/Header";
+import Link from "next/link";
+
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { TextPlugin } from "gsap/TextPlugin";
+
 
 export default function Contact() {
+  const testRef = useRef(null);
+
+  useEffect(() => {
+    gsap.to(testRef.current, {
+      delay: 1.5,
+      scrambleText: {
+        text: "I sure hope this works for you.",
+        rightToLeft: true,
+        chars: "lowercase",
+      },
+      duration: 5,
+    });
+  }, []);
+
+  gsap.registerPlugin(TextPlugin);
+
   return (
-    <div className="container mx-auto ">
+    <div className="">
       <Header />
-      <div className="flex  justify-center mt-[30px]">
-        <div className="text-7xl">Contact</div>
-      </div>
-      <div className="flex mt-[30px] px-[20px] w-full gap-8 justify-between">
-        <div className="w-6/12 flex gap-4 flex-wrap">
-          <div className="text-6xl">INSTAGRAM</div>
-          <div className="text-6xl">FACEBOOK</div>
-          <div className="text-6xl">EMAIL</div>
-          <div className="text-6xl">PHONE NUMBER</div>
+      <div className="container mx-auto  bg-gradient-to-r from-purple-500 to-pink-500">
+        <div className="flex  justify-center ">
+          <div className="text-7xl">Contact</div>
         </div>
-        <div className="w-6/12">
-          <div>PHOTO</div>
-          <button className="btn btn-active btn-secondary">Secondary</button>
+        <div className="flex mt-[30px] px-[20px] w-full gap-8 justify-center">
+          <div>
+            <div className="flex justify-center text-[30px]">Social</div>
+            <div className="text-[50px] flex flex-col">
+              <Link
+                className="link-hover"
+                href={`https://www.instagram.com/dominggo__/?__pwa=1`}
+              >
+                INSTAGRAM
+              </Link>
+              <Link
+                className="link-hover"
+                href={`https://www.facebook.com/batmunhk.batbold.9/`}
+              >
+                FACEBOOK
+              </Link>
+              <Link className="link-hover" href={`https://mail.google.com`}>
+                EMAIL
+              </Link>
+              <Link className="link-hover" href={``}>
+                PHONE NUMBER
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
