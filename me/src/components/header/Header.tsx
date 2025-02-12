5fe3064bd303bd268e0aebd295d1b1d228fb1915
dmngo <164151923+DMNgoboki@users.noreply.github.com>
2025-02-12 20:05:51 +0800

    fix

--- me/src/components/header/Header.tsx
+++ me/src/components/header/Header.tsx
@@ -7,7 +7,7 @@ import { headerlists } from "./HeaderLists";
 export function Header() {
   return (
     <div className="container mx-auto bg-transparent">
-      <div className="hidden md:hidden lg:flex font-anton text-4xl font-extrabold justify-around">
+      <div className="hidden md:hidden lg:flex font-anton text-4xl font-extrabold justify-around  2xl:text-6xl ">
         {headerlists.map((headerlist) => (
           <Link key={headerlist.id} href={headerlist.link}>
             <div>{headerlist.title}</div>
